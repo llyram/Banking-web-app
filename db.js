904c970b4b855e478d1c6327ca710caf386239ad
Maryll Castelino <maryllcastelino@gmail.com>
2021-05-12 16:46:47 +0530

    I am tired of this shit

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD
 const proConfig = process.env.DATABASE_URL;
 
 const pool = new Pool({
-  connectionString : process.env.NODE_ENV === "production" ? proConfig : devConfig
+  connectionString : process.env.NODE_ENV === "production" ? proConfig : devConfig,
 });
 
 
