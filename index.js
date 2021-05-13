81501a4d19b2d3cf4f63d3d3af17e143c6868968
Maryll Castelino <maryllcastelino@gmail.com>
2021-05-13 12:19:19 +0530

    using mock data to test backend

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ if (process.env.NODE_ENV === "production") {
 app.get("/getcustomers", async (req, res) => {
   console.log("query");
   try {
-    const allCustomers = await pool.query(
-      "SELECT * FROM customers ORDER BY c_id"
-    );
-    console.log("hello");
-    res.json(allCustomers.rows);
+    // const allCustomers = await pool.query(
+    //   "SELECT * FROM customers ORDER BY c_id"
+    // );
+    // console.log(allCustomers.rows);
+    res.json(mock_return);
   } catch (err) {
     console.log(err.message);
   }
@@ -68,3 +68,67 @@ app.get("/gethistory", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`server has started at port ${PORT}`);
 });
+
+
+const mock_return = [
+  {
+    c_id: 1,
+    name: 'Ermengarde Nickolls',
+    email: 'enickolls0@yahoo.com',
+    balance: 10684
+  },
+  {
+    c_id: 2,
+    name: 'Harmon Pughsley',
+    email: 'hpughsley1@cam.ac.uk',
+    balance: 41251
+  },
+  {
+    c_id: 3,
+    name: 'Noel Di Gregorio',
+    email: 'ndi2@addthis.com',
+    balance: 26718
+  },
+  {
+    c_id: 4,
+    name: 'Berty Balham',
+    email: 'bbalham3@nytimes.com',
+    balance: 42490
+  },
+  {
+    c_id: 5,
+    name: 'Kitty Troppmann',
+    email: 'ktroppmann4@symantec.com',
+    balance: 8592
+  },
+  {
+    c_id: 6,
+    name: 'Barrett Shankle',
+    email: 'bshankle5@sciencedaily.com',
+    balance: 65750
+  },
+  {
+    c_id: 7,
+    name: 'Luella Lapham',
+    email: 'llapham6@baidu.com',
+    balance: 28375
+  },
+  {
+    c_id: 8,
+    name: 'Tori Clyant',
+    email: 'tclyant7@vinaora.com',
+    balance: 54420
+  },
+  {
+    c_id: 9,
+    name: 'Nickey Boulding',
+    email: 'nboulding8@google.ru',
+    balance: 38044
+  },
+  {
+    c_id: 10,
+    name: 'Allina Simao',
+    email: 'asimao9@youtu.be',
+    balance: 39923
+  }
+]
